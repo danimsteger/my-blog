578e8e9fdfc98cf33cfcc1247cc36e8e4e529b9e
danimsteger <daniellestegertutoring@gmail.com>
2024-06-17 21:27:14 -0400

    added light and dark mode toggle

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,6 @@
 const backButton = document.querySelector("#back");
+const themeSwitcher = document.querySelector("#theme-switcher");
+const container = document.querySelector(".container");
 
 function back() {
   window.location.href = "./index.html";
@@ -9,3 +11,21 @@ backButton.addEventListener("click", back);
 testAppend();
 
 renderBlogs();
+
+let mode = "light";
+
+themeSwitcher.addEventListener("click", function () {
+  if (mode === "light") {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+    console.log("we are switching to dark mode");
+    themeSwitcher.textContent = "🌝";
+  } else {
+    mode = "light";
+    container.setAttribute("class", "light");
+    console.log("we are switching to light mode");
+    themeSwitcher.textContent = "🌚";
+  }
+});
+
+// alert("you clicked this");
